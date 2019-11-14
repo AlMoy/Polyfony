eb437fc4f5b729ad684622962ad0c1c5868649de
Alex Moysan <datiar0@gmail.com>
2019-11-14 16:43:27 +0100

    Création des entity

---
+++ EntityClass/Entity/Product.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityClass.Entity
+    {
+    public class Product
+        {
+        #region Attributs
+        private long id;
+        private float size;
+        private string name;
+        private float weight;
+        private string color;
+        private long quantity;
+        private List<StateProduct> stateProducts;
+        private List<ProductOrder> productOrders;
+        private ProductType productType;
+        #endregion
+
+        #region Properties
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Column("pr_id")]
+        public long Id
+            {
+            get { return id; }
+            set { id = value; }
+            }
+
+        [Required]
+        [Column("pr_name")]
+        [DataType(DataType.Text)]
+        [MinLength(4)]
+        [MaxLength(20)]
+        public string Name
+            {
+            get { return name; }
+            set { name = value; }
+            }
+
+        [Required]
+        [Column("pr_size")]
+        public float Size
+            {
+            get { return size; }
+            set { size = value; }
+            }
+
+        [Required]
+        [Column("pr_weight")]
+        public float Weight
+            {
+            get { return weight; }
+            set { weight = value; }
+            }
+
+        [Required]
+        [Column("pr_color")]
+        [DataType(DataType.Text)]
+        public string Color
+            {
+            get { return color; }
+            set { color = value; }
+            }
+
+        [Required]
+        [Column("pr_quantity")]
+        public long Quantity
+            {
+            get { return quantity; }
+            set { quantity = value; }
+            }
+
+        public List<StateProduct> StateProducts
+            {
+            get { return stateProducts; }
+            set { stateProducts = value; }
+            }
+
+        public List<ProductOrder> ProductOrders
+            {
+            get { return productOrders; }
+            set { productOrders = value; }
+            }
+
+        public ProductType ProductType
+            {
+            get { return productType; }
+            set { productType = value; }
+            }
+        #endregion
+
+        #region Constructors
+        public Product()
+            {
+            this.stateProducts = new List<StateProduct>();
+            }
+        #endregion
+        }
+    }
---
+++ EntityClass/Entity/Role.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityClass.Entity
+    {
+    public class Role
+        {
+        #region Attributs
+        private long id;
+        private string name;
+        private List<Person> people;
+        #endregion
+
+        #region Properties
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Column("ro_id")]
+        public long Id
+            {
+            get { return id; }
+            set { id = value; }
+            }
+
+        [Required]
+        [Column("ro_name")]
+        [DataType(DataType.Text)]
+        [MinLength(4)]
+        [MaxLength(20)]
+        public string Name
+            {
+            get { return name; }
+            set { name = value; }
+            }
+
+        public List<Person> People
+            {
+            get { return people; }
+            set { people = value; }
+            }
+        #endregion
+
+        #region Constructors
+        public Role()
+            {
+            this.people = new List<Person>();
+            }
+        #endregion
+        }
+    }
