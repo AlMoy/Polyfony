f897c01d4811b5b18bbea907a6e7e9d77b4211df
Alex Moysan <datiar0@gmail.com>
2019-12-15 17:04:08 +0100

    Ajout des tests TU0007 et TU0009

--- UnitTestASPNet/UnitTest1.cs
+++ UnitTestASPNet/UnitTest1.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using EntityASP;
+using EntityASP.Entity;
 using EntityASP.Repository;
 using NUnit.Framework;
 
@@ -25,6 +26,20 @@ namespace UnitTestASPNet
             List<T> list = repository.FindAll();
             Assert.IsEmpty(list);
             return list;
+            }
+
+        [Test]
+        public List<T> GetListFiltered<T>(Repository<T> repository, Dictionary<String, String> criteria, Dictionary<String, String> orderBy = null, ulong? limit = null, ulong? offset = null)
+            {
+            List<T> list = repository.FindBy(criteria, orderBy, limit, offset);
+            Assert.IsEmpty(list);
+            return list;
+            }
+
+        [Test]
+        public void ToAccessAdmin(Person person)
+            {
+            Assert.IsTrue(person.Role.Name == "admin");
             }
         }
     }
