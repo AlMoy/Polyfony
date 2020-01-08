99e62cb8e4b6567060a6ea712a507a32d08e6b65
Alex Moysan <moysan.alex@gmail.com>
2020-01-08 17:46:06 +0100

    Affichage des UC

---
+++ UWP_SellIt/Views/ProductPage/ListUC.xaml.cs
@@ -0,0 +1,38 @@
+﻿using EntityUWP.Entity;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// Pour en savoir plus sur le modèle d'élément Contrôle utilisateur, consultez la page https://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace UWP_SellIt.Views.ProductPage
+    {
+    public sealed partial class ListUC : UserControl
+        {
+        public ObservableCollection<Product> ProductList { get; set; }
+
+        public ListUC()
+            {
+            this.InitializeComponent();
+
+            this.ProductList = new ObservableCollection<Product>();
+            this.ProductList.Add(new Product() { Id = 1, Name = "Produit 1", Color = "Blue", Size = 2, Weight = 3, Quantity = 10, ToValid = true, ProductOrders = new List<ProductOrder>(), StateProducts = new List<StateProduct>(), ProductType = null });
+            this.ProductList.Add(new Product() { Id = 2, Name = "Produit 2", Color = "Rouge", Size = 1.2F, Weight = 2.5F, Quantity = 15, ToValid = true, ProductOrders = new List<ProductOrder>(), StateProducts = new List<StateProduct>(), ProductType = null });
+            this.ProductList.Add(new Product() { Id = 3, Name = "Produit 3", Color = "Jaune", Size = 3.2F, Weight = 1.5F, Quantity = 0, ToValid = true, ProductOrders = new List<ProductOrder>(), StateProducts = new List<StateProduct>(), ProductType = null });
+
+            this.DataContext = this;
+            }
+        }
+    }
